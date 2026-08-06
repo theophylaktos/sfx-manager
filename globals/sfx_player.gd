fae8742734f08c39fe188c2bc005092471cc1e83
Theophylaktos <91230260+theophylaktos@users.noreply.github.com>
2026-08-06 07:46:25 -0400

    update

--- globals/sfx_player.gd
+++ globals/sfx_player.gd
@@ -7,16 +7,16 @@ extends Node
 ##
 ## Use:
 ## When you want to add a new sound, start by loading the file into Godot.
-## Add a descriptive name to the enum "Labels" in sfx_player.gd. 
-## This name should be in ALL_CAPS
-## Go into the inspector of the node "SfxPlayer" in sfx_player.gd
-## In the dictionary "Label to Setting," add a new pair of a key and a setting
-## Select your audio file as the "stream"
+## Add a descriptive name to the enum "Labels" in sfx_player.gd.
+## This name should be in ALL_CAPS.
+## Go into the inspector of the node "SfxPlayer" in sfx_player.gd.
+## In the dictionary "Label to Setting," add a new pair of a key and a setting.
+## Select your audio file as the "stream."
 ## Make sure to click "Add Key/Value Pair"!!
-## You can now call SFX.play(SFX.Labels.YOUR_LABEL)!
+## You can now call SFX.play(SFX.Labels.YOUR_LABEL).
 
-## Optionally, append a comment (##) after each label to describe where it is used [br]
-## List of all sounds. Add a new item here when you add a new sound.
+# Optionally, append a comment (##) after each label to describe where it is used [br]
+## List of all sounds. Add a new label here when you add a new sound.
 enum Labels {
 	FALL, 
 	AMBIENCE,
